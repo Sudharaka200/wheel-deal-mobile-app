1466793e93eafc9e9e72cb4dfc96c88e73a85be7
ShashiKalum11 <COHNDSE241F-120@student.nibm.lk>
2025-02-05 13:11:23 +0530

    Create New Add UI

--- app/src/main/java/com/example/wheeldeal/createnewadd.java
+++ app/src/main/java/com/example/wheeldeal/createnewadd.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
